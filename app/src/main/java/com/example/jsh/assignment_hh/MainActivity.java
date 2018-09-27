81e4bf6e8cc03d26ee260a9e7f261a6b0db7aa8f
Herman <z5160172@ad.unsw.edu.au>
2018-09-27 16:45:00 +1000

    4:44pm holder method for intent

--- app/src/main/java/com/example/jsh/assignment_hh/MainActivity.java
+++ app/src/main/java/com/example/jsh/assignment_hh/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.jsh.assignment_hh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView Content = (TextView) findViewById(R.id.content);
+        TextView Flashcards = (TextView) findViewById(R.id.flashcards);
+        TextView Quiz = (TextView) findViewById(R.id.quiz);
+        TextView additionalResources = (TextView) findViewById(R.id.additional_resources);
+
+        View.OnClickListener menuOptions = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView t = (TextView) v;
+                String text = t.toString();
+                Toast.makeText(MainActivity.this, "You selected " + text, Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Content.setOnClickListener(menuOptions);
+        Flashcards.setOnClickListener(menuOptions);
+        Quiz.setOnClickListener(menuOptions);
+        additionalResources.setOnClickListener(menuOptions);
     }
+
+
+
+
 }
