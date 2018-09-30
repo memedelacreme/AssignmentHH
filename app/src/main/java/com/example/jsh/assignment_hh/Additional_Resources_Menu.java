d47be2251df776e6167791b1870fbea06704c3d5
Herman <z5160172@ad.unsw.edu.au>
2018-09-30 14:16:42 +1000

    30/09/2018 | 2:16pm Got a video into additional resources

--- app/src/main/java/com/example/jsh/assignment_hh/Additional_Resources_Menu.java
+++ app/src/main/java/com/example/jsh/assignment_hh/Additional_Resources_Menu.java
@@ -1,32 +1,138 @@
 package com.example.jsh.assignment_hh;
 
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class Additional_Resources_Menu extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Additional_Resources_Menu extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final String TAG = "Additional_Resources_M";
 
     public String YOUTUBE_API_KEY = "AIzaSyCqwpllIfLUAZFWhvCWbPQqWVAY6KbOeZ8";
+
+    private YouTubePlayerView youTubeView;
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener;
+    private YouTubePlayer.PlaybackEventListener playbackEventListener;
+    private YouTubePlayer youTubePlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additional__resources__menu);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        youTubeView = (YouTubePlayerView) findViewById(R.id.video1);
+        youTubeView.initialize(YOUTUBE_API_KEY, this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onLoading() {
+
             }
-        });
+
+            @Override
+            public void onLoaded(String s) {
+
+            }
+
+            @Override
+            public void onAdStarted() {
+
+            }
+
+            @Override
+            public void onVideoStarted() {
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+
+        };
+        playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+
+            }
+
+            @Override
+            public void onPaused() {
+
+            }
+
+            @Override
+            public void onStopped() {
+
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+
+            }
+        };
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+        this.youTubePlayer = youTubePlayer;
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+
+        if(!b){
+            youTubePlayer.cueVideo("YcbcfkLzgvs");
+        }
+
+        Log.d(TAG, "onInitializationSuccess: Provide is " + provider.getClass().toString() );
+        Log.d(TAG, "onInitializationSuccess: Playing video [INSERT NAME HERE]");
+        Toast.makeText(this, "Playing video [INSERT NAME HERE]", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        final int REQUEST_CODE = 1;
+
+        if (youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        }
+        else {
+            String error = String.format("There was an error intialising the YouTube Player (%1$s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+        }
+    }
 }
