5a7c25a2ae470c49e83c12d2a6802eec1cfef754
Henry <henryzhao16@gmail.com>
2018-09-30 15:13:30 +1000

    30/09/18 1:50PM Splash screen added

--- app/src/main/java/com/example/jsh/assignment_hh/MainActivity.java
+++ app/src/main/java/com/example/jsh/assignment_hh/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jsh.assignment_hh;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +12,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private static int SPLASH_TIME_OUT = 3000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //splash screen (screen that is displayed for brief 3 seconds before application home screen is reached) TODO: ADD LOGO
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(MainActivity.this, SplashActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
 
         TextView Content = (TextView) findViewById(R.id.content);
         TextView Flashcards = (TextView) findViewById(R.id.flashcards);
