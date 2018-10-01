4e2ee462a99f000e08f3c68abd29f9d3257333c3
Henry <henryzhao16@gmail.com>
2018-10-01 19:10:48 +1000

    30/09/18 6:42PM Fixed floating action button in previous commit.          7:08PM Added an OnClickListener with intents onto the floating action button, created AddFlashCard activity and set out basic layout

---
+++ app/src/main/java/com/example/jsh/assignment_hh/AddFlashCard.java
@@ -0,0 +1,13 @@
+package com.example.jsh.assignment_hh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddFlashCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_flash_card);
+    }
+}
