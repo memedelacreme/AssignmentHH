fc585e1520aae8c24b8ac46315aadd688b2181de
Henry <henryzhao16@gmail.com>
2018-10-01 23:27:07 +1000

    1/10/18 Updated AddFlashCard.java a bit

--- app/src/main/java/com/example/jsh/assignment_hh/AddFlashCard.java
+++ app/src/main/java/com/example/jsh/assignment_hh/AddFlashCard.java
@@ -2,12 +2,37 @@ package com.example.jsh.assignment_hh;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static com.example.jsh.assignment_hh.FlashCardMain.mFact;
+import static com.example.jsh.assignment_hh.FlashCardMain.mTopic;
 
 public class AddFlashCard extends AppCompatActivity {
+
+    Button addCard;
+    EditText t_input, f_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_flash_card);
+
+        addCard = findViewById(R.id.addCard);
+        t_input = findViewById(R.id.topic_input);
+        f_input = findViewById(R.id.fact_input);
+
+        addCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                mTopic.add(t_input.getText().toString());
+                mFact.add(f_input.getText().toString());
+
+
+                //TODO:How to display added cards? + Make it go back to the flash cards screen
+            }
+        });
     }
 }
+
