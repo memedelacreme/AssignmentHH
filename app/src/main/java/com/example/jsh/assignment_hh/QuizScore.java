c5ad7bba0a50394c3cf8d944f82e3b37735f6d99
Herman <z5160172@ad.unsw.edu.au>
2018-10-23 19:00:20 +1100

    23/10/2018 06:59PM Added a background for results screen

--- app/src/main/java/com/example/jsh/assignment_hh/QuizScore.java
+++ app/src/main/java/com/example/jsh/assignment_hh/QuizScore.java
@@ -1,8 +1,12 @@
 package com.example.jsh.assignment_hh;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class QuizScore extends AppCompatActivity {
@@ -15,13 +19,18 @@ public class QuizScore extends AppCompatActivity {
 
         TextView greeting = (TextView) findViewById(R.id.greeting);
         TextView score = (TextView) findViewById(R.id.score);
+        ImageView background = (ImageView) findViewById(R.id.scoreBackground);
+
+
 
         int quizScore = getIntent().getIntExtra("quizScore", 0);
 
         if (quizScore < 5) {
             greeting.setText("Oh dear... \n Looks like you need to do a bit more study!");
+
         } else if (quizScore >= 5 && quizScore < 8) {
             greeting.setText("Good job! \n A little bit more study and you'll be good to go!");
+
         } else if (quizScore >= 8) {
             greeting.setText("Excellent Work!");
         }
